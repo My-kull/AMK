b35c41e7a4f7c5797e33a147fed412ed693a2e0b
My-kull <hurst-maxims.0m@icloud.com>
2025-04-10 13:43:19 +0300

    Fixed reverseList()

--- AMK_koodit/JavaScript/JSmod/src/JS/JSmod2.js
+++ AMK_koodit/JavaScript/JSmod/src/JS/JSmod2.js
@@ -7,10 +7,14 @@ function reverseList() {
   for (let i = 0; i < 5; i++) {
     numbers.push(parseInt(prompt("Enter a number:")));
   }
-  console.log(numbers.reverse());
+  for (let i = 5; i > 0; i--) {
+    console.log(numbers[i - 1]);
+  }
   document.querySelector("#reverseListTarget").innerHTML =
     "Check JavaScript console!";
 }
+
+// Tehtävä 2.2
 
 export default {
   reverseList,
